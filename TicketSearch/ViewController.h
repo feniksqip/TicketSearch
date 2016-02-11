cfb04d92fea8cdf4e1c861ee42dcff3d06ea014d
Andrey <andrey@mac-admin.local>
2016-02-11 21:06:53 +0300

    Initial Commit

---
+++ TicketSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketSearch
+//
+//  Created by Andrey on 11.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicketSearchTests/TicketSearchTests.m
@@ -0,0 +1,39 @@
+//
+//  TicketSearchTests.m
+//  TicketSearchTests
+//
+//  Created by Andrey on 11.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TicketSearchTests : XCTestCase
+
+@end
+
+@implementation TicketSearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
